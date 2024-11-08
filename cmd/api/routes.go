6bd5d3781b457b82477d41102778cd01b7633229
gittesting <120082611+Fenr1sulfr@users.noreply.github.com>
2024-11-08 23:28:42 +0500

    Added new handlers for password reset(Updating routes)

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"expvar"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
@@ -17,6 +18,9 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/tokens/password-reset", app.createPasswordResetTokenHandler)
 
+	router.Handler(http.MethodGet, "/debug/vars", expvar.Handler())
 	return app.metrics(app.recoverPanic(app.enableCORS(app.rateLimit(app.authenticate(router)))))
 }
+ 
--- cmd/api/users.go
+++ cmd/api/users.go
@@ -8,6 +8,65 @@ import (
 	"time"
 )
 
+func (app *application) updateUserPasswordHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Password       string `json:"password"`
+		TokenPlainText string `json:"token"`
+	}
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.serverErrorRespone(w, r, err)
+		return
+	}
+	v := validator.New()
+
+	data.ValidatePassowrdPlaintext(v, input.Password)
+	data.ValidateTokenPlaintext(v, input.TokenPlainText)
+	if !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+	user, err := app.models.Users.GetForToken(data.ScopePasswordReset, input.TokenPlainText)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrNoRecordFound):
+			v.AddError("token", "invalid or expired reset token")
+			app.failedValidationResponse(w, r, v.Errors)
+		default:
+			app.serverErrorRespone(w, r, err)
+		}
+		return
+	}
+	err = user.Password.Set(input.Password)
+	if err != nil {
+		app.serverErrorRespone(w, r, err)
+		return
+	}
+	err = app.models.Users.Update(user)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrEditConflict):
+			app.editConflictResponse(w, r)
+		default:
+			app.serverErrorRespone(w, r, err)
+
+		}
+		return
+	}
+
+	err = app.models.Tokens.DeleteAllForUser(data.ScopePasswordReset, user.ID)
+	if err != nil {
+		app.serverErrorRespone(w, r, err)
+		return
+	}
+	env := envelope{"message": "your password was succesfuly reset"}
+	err = app.writeJSON(w, http.StatusAccepted, env, nil)
+	if err != nil {
+		app.serverErrorRespone(w, r, err)
+	}
+}
+
 func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
 		Name     string `json:"name"`
